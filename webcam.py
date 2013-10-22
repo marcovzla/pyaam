521f80142148bf894c5227078bff227d8dcc202e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2013-10-22 15:40:30 -0700

    added script to find face in webcam video

---
+++ webcam.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import division
+
+import sys
+import cv2
+import cv2.cv as cv
+
+cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+
+cv2.namedWindow('webcam')
+cam = cv2.VideoCapture(0)
+
+if not cam.isOpened():
+    sys.exit('no camera')
+
+while True:
+    rval, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    rects = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4,
+                                     minSize=(30,30), flags=cv.CV_HAAR_SCALE_IMAGE)
+
+    for x,y,w,h in rects:
+        cv2.rectangle(img, (x,y), (x+w,y+h), cv.RGB(0,0,255), 2)
+
+    cv2.imshow('webcam', img)
+
+    if cv2.waitKey(5) == 27:
+        break
