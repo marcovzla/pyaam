2ac070163b2edb6b85ba320d8d5815e298c0bebb
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2013-10-21 22:39:09 -0700

    created view_shape_model() in view_model.py

--- view_model.py
+++ view_model.py
@@ -24,36 +24,37 @@ def parse_args():
 
 
 
-if __name__ == '__main__':
-    args = parse_args()
-
-    smodel = ShapeModel.load(args.shp_fn)
-
-    img = np.empty((args.height, args.width, 3), dtype='uint8')
-
+def genvals():
     vals = np.empty(200)
     vals[:50] = np.arange(50) / 50
     vals[50:100] = (50 - np.arange(50)) / 50
     vals[100:] = -vals[:100]
+    return vals
 
+
+
+def view_shape_model(shp_fn, scale, tranx, trany, width, height):
+    img = np.empty((height, width, 3), dtype='uint8')
+    smodel = ShapeModel.load(shp_fn)
+    vals = genvals()
     while True:
         for k in xrange(4, smodel.model.shape[1]):
             for v in vals:
-                p = smodel.get_params(args.scale, args.tranx, args.trany)
+                p = smodel.get_params(scale, tranx, trany)
                 p[k] = p[0] * v * 3 * np.sqrt(smodel.variance[k])
-
-                img[:] = 0
-
+                img[:] = 0  # set to black
                 s = 'mode: %d, val: %f sd' % (k-3, v*3)
                 draw_string(img, s)
-
                 q = smodel.calc_shape(p)
-
                 pts = np.column_stack((q[::2], q[1::2]))
-
                 draw_pairs(img, pts, MuctDataset.PAIRS, Color.red)
                 draw_points(img, pts, Color.green)
-
                 cv2.imshow('shape model', img)
                 if cv2.waitKey(10) == 27:
                     sys.exit()
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    view_shape_model(args.shp_fn, args.scale, args.tranx, args.trany, args.width, args.height)
