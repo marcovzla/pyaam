cb5dd4859180c53cbf2dd7c6e6df7090ba08b1c1
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2013-10-28 18:34:15 -0700

    use argparse in train.py

--- train.py
+++ train.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
 import os
+import argparse
 from pyaam.muct import MuctDataset
 from pyaam.shape import ShapeModel
 
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--frac', type=float, default=0.99, help='')
+    parser.add_argument('--kmax', type=int, default=20, help='')
+    parser.add_argument('--shp-fn', default='data/shape.npz', help='shape model filename')
+    return parser.parse_args()
+
+
+
 if __name__ == '__main__':
+    args = parse_args()
+
     muct = MuctDataset()
     muct.load(clean=True)
-
-    frac = 0.99
-    kmax = 20
-    filename = 'data/shape.npz'
 
     if not os.path.exists('data'):
         os.makedirs('data')
@@ -18,8 +28,8 @@ if __name__ == '__main__':
     data = muct.all_lmks()
 
     print 'shape model training samples:', len(data)
-    smodel = ShapeModel.train(data.T, frac, kmax)
+    smodel = ShapeModel.train(data.T, args.frac, args.kmax)
     print 'retained:', smodel.num_modes(), 'modes'
-    smodel.save(filename)
-    print 'wrote', filename
+    smodel.save(args.shp_fn)
+    print 'wrote', args.shp_fn
 
