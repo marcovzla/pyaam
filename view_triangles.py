ddc76aeff6c748d31c4faebfe183b2f99cb9d4fc
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2013-11-18 18:56:09 -0700

    added cmd line args

--- view_triangles.py
+++ view_triangles.py
@@ -13,11 +13,20 @@ from pyaam.utils import get_aabb, normalize, warp_triangles, get_vertices
 
 
 
-if __name__ == '__main__':
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--no-tm', dest='use_tm', action='store_false',
+    parser.add_argument('--no-tm', dest='use_texturemapper', action='store_false',
                         help='disable TextureMapper')
-    args = parser.parse_args()
+    parser.add_argument('--no-norm', dest='normalize', action='store_false',
+                        help='disable roi normalization')
+    parser.add_argument('--no-trigs', dest='show_triangles', action='store_false',
+                        help='hide triangles')
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
 
     cv2.namedWindow('triangles')
     cv2.namedWindow('warp')
@@ -32,7 +41,7 @@ if __name__ == '__main__':
     muct = MuctDataset()
     muct.load(clean=True)
 
-    if args.use_tm:
+    if args.use_texturemapper:
         from pyaam.texturemapper import TextureMapper
         tm = TextureMapper(480, 640)
         warp_triangles = tm.warp_triangles
@@ -41,12 +50,15 @@ if __name__ == '__main__':
         img = muct.image(name)
         pts = lmks.reshape((len(lmks)//2, 2))
         aabb = get_aabb(pts)
-        normalize(img, aabb)
+        if args.normalize:
+            normalize(img, aabb)
         orig = img.copy()
-        draw_polygons(img, pts[verts], Color.blue)
+        if args.show_triangles:
+            draw_polygons(img, pts[verts], Color.blue)
         cv2.imshow('triangles', img)
         warped = warp_triangles(orig, pts[verts], ref[verts])
-        draw_polygons(warped, ref[verts], Color.blue)
+        if args.show_triangles:
+            draw_polygons(warped, ref[verts], Color.blue)
         cv2.imshow('warp', warped[:300,:300])
         key = cv2.waitKey()
         if key == 27:
